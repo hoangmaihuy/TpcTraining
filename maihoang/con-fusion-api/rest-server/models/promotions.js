fb1dbf4e73e2e12aa750c51d031d07cdb0dd577d
Mai Huy Hoang <pynhp9x@gmail.com>
2016-08-01 23:25:17 +0700

    Done Lesson 1

---
+++ maihoang/con-fusion-api/rest-server/models/promotions.js
@@ -0,0 +1,31 @@
+var mongoose = require('mongoose');
+require('mongoose-currency').loadType(mongoose);
+var Schema = mongoose.Schema;
+var Currency = mongoose.Types.Currency;
+
+var promotionSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    label: {
+        type: String,
+        default: ''
+    },
+    price: {
+        type: Currency,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+});
+
+var Promotions = mongoose.model('Promotion', promotionSchema);
+module.exports = Promotions;
