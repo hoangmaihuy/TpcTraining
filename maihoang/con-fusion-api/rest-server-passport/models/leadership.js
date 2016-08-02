162de6e009c8aa01d2e2438436e05828b9d8edf0
Mai Huy Hoang <pynhp9x@gmail.com>
2016-08-03 02:11:45 +0700

    Done Exercise 4

---
+++ maihoang/con-fusion-api/rest-server-passport/models/leadership.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var leaderSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    designation: {
+        type: String,
+        required: true
+    },
+    abbr: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+});
+
+var Leadership = mongoose.model('Leader', leaderSchema);
+module.exports = Leadership;
