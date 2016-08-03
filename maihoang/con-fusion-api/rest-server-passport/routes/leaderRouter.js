9259e1e79aa16351d8edf81ccd7543e18550554e
Mai Huy Hoang <pynhp9x@gmail.com>
2016-08-03 15:31:18 +0700

    Finish NodeJS Week 3

--- maihoang/con-fusion-api/rest-server-passport/routes/leaderRouter.js
+++ maihoang/con-fusion-api/rest-server-passport/routes/leaderRouter.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var Verify = require('./verify');
 
 var Leadership = require('../models/leadership');
 
@@ -8,56 +9,57 @@ var leaderRouter = express.Router();
 leaderRouter.use(bodyParser.json());
 
 leaderRouter.route('/')
-.get(function (req, res, next) {
-    Leadership.find({}, function (err, leader) {
-        if (err) throw err;
-        res.json(leader);
+  .get(Verify.verifyOrdinaryUser, function(req, res, next) {
+    Leadership.find({}, function(err, leader) {
+      if (err) throw err;
+      res.json(leader);
     });
+  })
+
+.post(Verify.verifyAdmin, function(req, res, next) {
+  Leadership.create(req.body, function(err, leader) {
+    if (err) throw err;
+    console.log('Leader created!');
+    var id = leader._id;
+
+    res.writeHead(200, {
+      'Content-Type': 'text/plain'
+    });
+    res.end('Added the leader with id: ' + id);
+  });
 })
 
-.post(function (req, res, next) {
-    Leadership.create(req.body, function (err, leader) {
-        if (err) throw err;
-        console.log('Leader created!');
-        var id = leader._id;
-
-        res.writeHead(200, {
-            'Content-Type': 'text/plain'
-        });
-        res.end('Added the leader with id: ' + id);
-    });
-})
-
-.delete(function (req, res, next) {
-    Leadership.remove({}, function (err, resp) {
-        if (err) throw err;
-        res.json(resp);
-    });
+.delete(Verify.verifyAdmin, function(req, res, next) {
+  Leadership.remove({}, function(err, resp) {
+    if (err) throw err;
+    res.json(resp);
+  });
 });
 
 leaderRouter.route('/:leaderId')
-.get(function (req, res, next) {
-    Leadership.findById(req.params.leaderId, function (err, leader) {
-        if (err) throw err;
-        res.json(leader);
+  .get(Verify.verifyOrdinaryUser, function(req, res, next) {
+    Leadership.findById(req.params.leaderId, function(err, leader) {
+      if (err) throw err;
+      res.json(leader);
     });
+  })
+
+.put(Verify.verifyAdmin, function(req, res, next) {
+  Leadership.findByIdAndUpdate(req.params.leaderId, {
+    $set: req.body
+  }, {
+    new: true
+  }, function(err, leader) {
+    if (err) throw err;
+    res.json(leader);
+  });
 })
 
-.put(function (req, res, next) {
-    Leadership.findByIdAndUpdate(req.params.leaderId, {
-        $set: req.body
-    }, {
-        new: true
-    }, function (err, leader) {
-        if (err) throw err;
-        res.json(leader);
-    });
-})
-
-.delete(function (req, res, next) {
-    Leadership.findByIdAndRemove(req.params.leaderId, function (err, resp) {        if (err) throw err;
-        res.json(resp);
-    });
+.delete(Verify.verifyAdmin, function(req, res, next) {
+  Leadership.findByIdAndRemove(req.params.leaderId, function(err, resp) {
+    if (err) throw err;
+    res.json(resp);
+  });
 });
 
 module.exports = leaderRouter;
--- maihoang/con-fusion-api/rest-server-passport/routes/verify.js
+++ maihoang/con-fusion-api/rest-server-passport/routes/verify.js
@@ -34,3 +34,27 @@ exports.verifyOrdinaryUser = function(req, res, next) {
     return next(err);
   }
 };
+
+exports.verifyAdmin = function(req, res, next) {
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+  if (token) {
+    jwt.verify(token, config.secretKey, function(err, decoded) {
+      if (err) {
+        var err = new Error('You are not authenticated!');
+        err.status = 401;
+        return next(err);
+      } else {
+        req.decoded = decoded;
+        if (!req.decoded._doc.admin) {
+          var err = new Error('You are not an admin!');
+          err.status = 403;
+          return next(err);
+        } else next();
+      }
+    });
+  } else {
+    var err = new Error('No token provided!');
+    err.status = 403;
+    return next(err);
+  }
+}
